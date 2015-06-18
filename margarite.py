387b37ed7740dfabb7882528c2920d5cc217cfee
Jun Sumida <jun.sumida@mixi.co.jp>
2015-06-18 22:38:05 +0900

    get install count

--- margarite.py
+++ margarite.py
@@ -2,6 +2,7 @@ from facebookads.api import FacebookAdsApi
 from facebookads import objects
 import yaml
 import json
+from datetime import date, timedelta
 
 from flask import Flask, jsonify, abort, Response
 app = Flask(__name__)
@@ -20,6 +21,21 @@ def ad_accounts():
     my_accounts = list(me.get_ad_accounts())
     accounts = map(lambda ad_account: ad_account['id'], my_accounts)
     response = Response(json.dumps(accounts), mimetype='application/json')
+
+    yesterday_date_string = (date.today() - timedelta(days=1)).isoformat()
+
+    insights = my_accounts[1].get_report_stats(params={"date_preset":'yesterday',"data_columns":["reach","spend","adgroup_id","account_name","clicks","actions"],"actions_group_by":["action_device","action_type"]})
+    install_count = 0
+    for insight in insights:
+        for action in insight["actions"]:
+            if action["action_type"] == "mobile_app_install":
+                install_count += action["value"]
+            else:
+                print ""
+        print ""
+    print ""
+
+    response = Response(json.dumps({"install_count":install_count, "date":yesterday_date_string}), mimetype='application/json')
 
     return response
 
